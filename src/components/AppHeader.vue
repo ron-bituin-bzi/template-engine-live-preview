f2e815759fbcef09a69f6d079de65239396b8ae9
Ronie Bituin <ronie.bituin@bzi.com.au>
2025-12-18 15:44:31 +1100

    break down other parts of the web app into components

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-app-bar density="compact" class="justify-space-between" elevation="2">
+    <v-app-bar-title>Template Engine Live Preview</v-app-bar-title>
+    <v-btn
+      :icon="theme.global.current.value.dark ? 'mdi-weather-sunny' : 'mdi-moon-waning-crescent'"
+      @click="toggleTheme"
+      variant="text"
+    />
+  </v-app-bar>
+</template>
+
+<script setup>
+  import { useTheme } from '@/composables/useTheme'
+
+  const { theme, toggleTheme } = useTheme()
+</script>
